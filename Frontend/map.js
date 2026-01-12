312852fbe4980c206b9cecf6111abda903344841
HugeCabbage69 <139630387+HugeCabbage69@users.noreply.github.com>
2026-01-12 23:54:03 +0530

    Added a blurry map to the background. (With attribution to leaflet)
    made the front page

---
+++ Frontend/map.js
@@ -0,0 +1,26 @@
+const map = L.map("map", {
+  dragging: false,
+  touchZoom: false,
+  scrollWheelZoom: false,
+  doubleClickZoom: false,
+  boxZoom: false,
+  keyboard: false,
+  zoomControl: false
+}).setView([0, 0], 17);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors"
+}).addTo(map);
+
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      map.setView([lat, lng], 17);
+    },
+    () => alert("No perm")
+  );
+}else {
+  alert("tatamas geo?");
+}
