5339d1dbc5aff3da765048865aa00a0caac4e3fd
h3kler0707 <sushantkumar8696@gmail.com>
2026-01-14 22:51:36 +0530

    added file to store marked areas and added login router to main.py

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from routes import otp_route,register
+from routes import otp_route,register,login
 
 app = FastAPI()
 
 app.include_router(otp_route.router)
 app.include_router(register.router)
+app.include_router(login.router)
